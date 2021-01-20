c15482c492c24b57d438894a652132faaca6de76
james reantillo <james.reantillo@gmail.com>
2021-01-20 14:49:16 -0800

    add state/tasks

---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+const Button = ({ color, text, onClick }) => {
+  return (
+    <button
+      className='btn'
+      style={{ backgroundColor: color }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+Button.defaulProps = {
+  color: 'steelblue',
+};
+
+Button.prototype = {
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+};
+export default Button;
